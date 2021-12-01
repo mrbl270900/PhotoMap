05faa259e422b4139482ad47dbefdc9cd0070eb6
LSHarder <90760113+LSHarder@users.noreply.github.com>
2021-12-01 14:15:03 +0100

    Added infowindow to marker

--- app/src/main/java/com/example/photomap/MapsActivity.java
+++ app/src/main/java/com/example/photomap/MapsActivity.java
@@ -2,22 +2,30 @@ package com.example.photomap;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.photomap.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
-
+    private static final LatLng RUC = new LatLng(55.652330724, 12.137999448);
+    private Marker mRUC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +38,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
+        // These are both viewgroups containing an ImageView with id "badge" and two TextViews with id
+        // "title" and "snippet".
+        private final View mWindow;
+
+        private final View mContents;
+
+        CustomInfoWindowAdapter() {
+            mWindow = getLayoutInflater().inflate(R.layout.custom_info_window, null);
+            mContents = getLayoutInflater().inflate(R.layout.custom_info_contents, null);
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            render(marker, mWindow);
+            return mWindow;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            render(marker, mContents);
+            return mContents;
+        }
+
+        private void render(Marker marker, View view) {
+            int badge = 0;
+            // Use the equals() method on a Marker to check for equals.  Do not use ==.
+            if (marker.equals(RUC)) {
+                badge = R.drawable.ic_launcher_foreground;
+            }
+            ((ImageView) view.findViewById(R.id.badge)).setImageResource(badge);
+
+        }
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -43,18 +85,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter());
+        mMap.setOnInfoWindowClickListener(this);
         float zoomLevel = 14.0f;
         // Add a marker in Sydney and move the camera
-        LatLng RUC = new LatLng(55.652330724, 12.137999448);
-        mMap.addMarker(new MarkerOptions().position(RUC).title("Marker ved RUC"));
+
+        //mMap.addMarker(new MarkerOptions().position(RUC).title("Marker ved RUC"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(RUC, zoomLevel));
         mMap.setOnMarkerClickListener(this);
+        mRUC = mMap.addMarker(new MarkerOptions().position(RUC).title("Marker ved RUC").infoWindowAnchor(0.5f, 0.5f));
     }
 
     @Override
     public boolean onMarkerClick(Marker marker) {
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 14.0f));
+        marker.showInfoWindow();
         return true;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
     }
 }
