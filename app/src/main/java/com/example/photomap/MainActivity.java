7cbc12bbb324f7fb4f5aa7439b03d97bcc9e8047
LSHarder <90760113+LSHarder@users.noreply.github.com>
2021-12-06 12:59:43 +0100

    henter billeder fra tlf

--- app/src/main/java/com/example/photomap/MainActivity.java
+++ app/src/main/java/com/example/photomap/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.photomap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    private int SELECT_IMAGE;
+    private ImageView img;
     Button minknap;
     TextView hello;
     Button minknap2;
@@ -30,7 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         minknap3.setOnClickListener(this);
 
         hello = findViewById(R.id.textView);
-
+        img = (ImageView)findViewById(R.id.imageView2);
     }
 
     @Override
@@ -39,9 +50,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(new Intent(MainActivity.this, MapsActivity.class));
         }else if(v == minknap2){
             hello.setText("hej med dig");
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"),SELECT_IMAGE);
         }else if(v == minknap3){
             getSupportFragmentManager().beginTransaction().add(R.id.frameLayout, new SettingsFragment()).commit();
 
         }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_IMAGE) {
+                Uri selectedImageUri = data.getData();
+                String selectedImagePath = getPath(selectedImageUri);
+                System.out.println("Image Path : " + selectedImagePath);
+                img.setImageURI(selectedImageUri);
+            }
+        }
+    }
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
     }
 }
