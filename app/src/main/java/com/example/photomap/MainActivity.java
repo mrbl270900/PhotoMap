5a3745ff32f1ac26f9f0045618dec8ef6e0a4915
madsrbl270900 <madsrbl@outlook.dk>
2021-12-13 12:42:10 +0100

    log in med firebase lavet på main

--- app/src/main/java/com/example/photomap/MainActivity.java
+++ app/src/main/java/com/example/photomap/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.photomap;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,9 +11,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -19,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView hello;
     Button minknap2;
     Button minknap3;
+    List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +52,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         hello = findViewById(R.id.textView);
         img = findViewById(R.id.imageView2);
+
+
     }
 
     @Override
@@ -43,10 +61,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(v == minknap){
             startActivity(new Intent(MainActivity.this, MapsActivity.class));
         }else if(v == minknap2){
-            hello.setText("hej med dig");
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Du er logget ud", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    });
         }else if(v == minknap3){
-            getSupportFragmentManager().beginTransaction().add(R.id.frameLayout, new SettingsFragment()).commit();
+            //getSupportFragmentManager().beginTransaction().add(R.id.frameLayout, new SettingsFragment()).commit();
+            // Create and launch sign-in intent
+            Intent signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build();
+            signInLauncher.launch(signInIntent);
+        }
+    }
 
+
+    // See: https://developer.android.com/training/basics/intents/result
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
         }
     }
 }
